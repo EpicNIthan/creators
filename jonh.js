8a4b087d24061be88f8f1f22ca3d00c116c943b9
EpicNIthan <love44ever00@gmail.com>
2025-12-24 21:52:50 +0700

    Add player object for Jonh with initial properties

---
+++ jonh.js
@@ -0,0 +1,11 @@
+// ===== Player: Jonh =====
+const jonh = {
+  name: "Jonh",
+  totalPaid: 0,
+  invitedBy: null,
+  commission: 0,
+  videos: [
+    "+50%https://youtube.com/shorts/8NxRQ_upRug?feature=share"
+  ],
+  img: "https://baleinesdansnausicaa.home.blog/wp-content/uploads/2019/05/img_1630-4.png"
+};
