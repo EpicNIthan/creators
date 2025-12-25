f2cfb057ebe1fc783f91d0caa8311db89855909f
EpicNIthan <love44ever00@gmail.com>
2025-12-25 07:23:56 +0700

    Update EpicNithan.js

--- EpicNithan.js
+++ EpicNithan.js
@@ -1,27 +1,9 @@
-// ===== Player: Itz.nooh =====
-window.itzNooh = {
-  name: "Itz.nooh",
-  totalPaid: 0.001,
-  invitedBy: "Epic_Nithan",
-  commission: 0,
-  balanceplus: 27,
-  videos: [
-    "https://youtube.com/shorts/DjREZ5xzBNs?feature=share",
-    "https://youtube.com/shorts/8NxRQ_upRug?feature=share",
-    "+5%https://youtube.com/shorts/O7jhzCOqmOY?feature=share",
-    "+10%https://youtube.com/shorts/HwY0gqJBcxM?feature=share",
-    "+5%https://youtube.com/shorts/WHc5nOvZeBg?feature=share"
-  ],
-  img: "https://static.wikia.nocookie.net/f0876b72-1368-473f-b475-0b2c77513d26/scale-to-width/755"
-};
-
 // ===== Player: Epic_Nithan =====
-window.epicNithan = {
+const epicNithan = {
   name: "Epic_Nithan",
   totalPaid: 0,
   invitedBy: "Jonh",
   commission: 0,
-  balanceplus: 0,
   videos: [
     "+100%https://youtube.com/shorts/wZeKy4HPM0g?feature=share",
     "+100%https://youtube.com/shorts/tfvl_BiesEo?feature=share",
@@ -35,17 +17,3 @@ window.epicNithan = {
   ],
   img: "https://cdn.brawlfind.com/profiles/28000214.webp"
 };
-
-// ===== Player: Jonh =====
-window.jonh = {
-  name: "Jonh",
-  totalPaid: 0,
-  invitedBy: null,
-  commission: 0,
-  balanceplus: 0,
-  videos: [
-    "+50%https://youtube.com/shorts/8NxRQ_upRug?feature=share"
-  ],
-  img: "https://baleinesdansnausicaa.home.blog/wp-content/uploads/2019/05/img_1630-4.png"
-};
-
